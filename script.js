1e9f2e1a3dd87996ff06e78bed710e5b7319e010
Hamza-ET <hamzamurad4499@gmail.com>
2024-08-29 15:51:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('noteForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+    const message = `*${title}*\n\n${content}\n\nView the notes online: Lecture Notes`;
+
+    fetch('https://api.telegram.org/bot<7173102809:AAFTsggdavbeS1xF3KM1PqHs1Xegvb0rDns>/sendMessage', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            chat_id: '-1002236654606',
+            text: 'https://<Hamza-ET>.github.io/<ET_Tutorial_webApp>/',
+            parse_mode: 'Markdown'
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.ok) {
+            alert('Note sent successfully!');
+        } else {
+            alert('Failed to send note.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while sending the note.');
+    });
+});
